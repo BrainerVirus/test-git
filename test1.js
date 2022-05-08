9ed2c3b1242adf404f0825a3f2378c08d4677196
Cristhofer Pincetti <cristhoferpincetti@hotmail.cl>
2022-05-08 21:06:01 +0000

    Mostrando estatus corto

--- test1.js
+++ test1.js
@@ -6,3 +6,12 @@ function calculateDaysBeteebDates(begin, end) {
   var days = millisBetween / millisecondsPerDay;
   return days;
 }
+
+function calculateDaysBeteebDates(begin, end) {
+  var beginDate = new Date(begin);
+  var endDate = new Date(end);
+  var millisecondsPerDay = 1000 * 60 * 60 * 24;
+  var millisBetween = endDate.getTime() - beginDate.getTime();
+  var days = millisBetween / millisecondsPerDay;
+  return days;
+}
