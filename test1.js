0d146c9a5935c85a3bb549903ce37ef5b93be409
Cristhofer Pincetti <cristhoferpincetti@hotmail.cl>
2022-05-08 21:15:05 +0000

    test1.js

--- test1.js
+++ test1.js
@@ -6,12 +6,3 @@ function calculateDaysBeteebDates(begin, end) {
   var days = millisBetween / millisecondsPerDay;
   return days;
 }
-
-function calculateDaysBeteebDates(begin, end) {
-  var beginDate = new Date(begin);
-  var endDate = new Date(end);
-  var millisecondsPerDay = 1000 * 60 * 60 * 24;
-  var millisBetween = endDate.getTime() - beginDate.getTime();
-  var days = millisBetween / millisecondsPerDay;
-  return days;
-}
